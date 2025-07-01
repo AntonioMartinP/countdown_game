745d211c3f2eac0df1ee5be0aab847ceff10e154
AntonioMartinP <anton28320github@gmail.com>
2025-07-01 13:51:26 +0200

    dialog

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,10 +1,18 @@
 import Player from './Components/Player.jsx'
+import TimerContainer from './Components/TimerContainer.jsx';
+import Timer from './Components/Timer.jsx';
 
 function App() {
 
   return (
     <>
       <Player />
+      <TimerContainer />
+      <Timer title="Nivel facil" time="1"></Timer>
+      <Timer title="Nivel intermedio" time="5"></Timer>
+      <Timer title="Nivel dificil" time="40"></Timer>
+      <Timer title="Nivel reto" time="60"></Timer>
+      
     </>
   )
 }
