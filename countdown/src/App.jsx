28932a7d373725449b0b899e9aeb9dc4c0e2c0c3
AntonioMartinP <josoner371@exitbit.com>
2025-07-09 05:39:04 +0200

    Improvements ModalDialog

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   return (
     <>
       <Player />
-      <TimerContainer />
-      <Timer title="Nivel facil" time="1"></Timer>
-      <Timer title="Nivel intermedio" time="5"></Timer>
-      <Timer title="Nivel dificil" time="40"></Timer>
-      <Timer title="Nivel reto" time="60"></Timer>
-      
+      <TimerContainer>
+        <Timer title="Nivel facil" time="1"></Timer>
+        <Timer title="Nivel intermedio" time="5"></Timer>
+        <Timer title="Nivel dificil" time="40"></Timer>
+        <Timer title="Nivel reto" time="60"></Timer>
+      </TimerContainer>
     </>
-  )
+ )
 }
 
 export default App;
--- countdown/src/Components/Timer.jsx
+++ countdown/src/Components/Timer.jsx
@@ -2,55 +2,66 @@ import { useRef, useState } from "react";
 import ModalDialog from "./ModalDialog";
 
 
-const Timer = ({title, time}) =>{
+const Timer = ({ title, time }) => {
 
-    const timer = useRef();
-    const dialogModal = useRef();
+  const initialTime = time * 1000;
+  const [timeRemaining, setTimeRemaining] = useState(initialTime);
 
-    const [hasLooser, setHasLooser] = useState(false);
-    const [hasStart, setHasStart] = useState(false);
+  const timer = useRef();
+  const dialogModal = useRef();
 
+  const timeIsRunning = timeRemaining >= 0 && timeRemaining < initialTime;
+  
+  console.log(timeIsRunning, timeRemaining, initialTime);
+  console.log(`diferencia= ${initialTime - timeRemaining}`);
     
 
-    const handleStartTime = () => {
-        setHasStart(true);
-        timer.current = setTimeout(()=>{
-            dialogModal.current.open();
-            setHasStart(false);
-            setHasLooser(true);
-        }, time * 1000)
-  
-    }
 
-    const handleStopTime = () => {
-      setHasStart(false);
-      clearInterval(timer.current);
-    }
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(initialTime);
+    dialogModal.current.open();
+  }
 
-    return (
-      <>
-        <ModalDialog ref={dialogModal} resultGame="perdido" timeTarget={time}/>
-        <section
-          className='w-[22rem] flex flex-col items-center justify-center p-8 m-8 
+  const onResetTimeRemaining = () => {
+    setTimeRemaining(initialTime);
+  }
+
+  const handleStartTime = () => {
+    timer.current = setInterval(() => {
+      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 100);
+    }, 100)
+
+  }
+
+  const handleStopTime = () => {
+    clearInterval(timer.current);
+    dialogModal.current.open();
+  }
+
+  return (
+    <>
+      <ModalDialog ref={dialogModal} timeRemaining={timeRemaining} timeTarget={time} onResetTimeRemaining={onResetTimeRemaining} />
+      <section
+        className='w-[22rem] flex flex-col items-center justify-center p-8 m-8 
             bg-[#691A1A] text-[##221c18] shadow-lg rounded-md'>
-          <h2 className='text-xl tracking-wide text-center uppercase text-[#edfcfa] font-bold mb-2'>
-            {title}
-          </h2>
-          {hasLooser && <p className='my-1 mb-3'>Has perdido</p>}
-          <p className='border-[1px] border-solid border-[#46cebe] rounded px-2 py-1 m-'>
-            {time} segundo{time > 1 ? 's' : ''}
-          </p>
-          <p
-            className='mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] 
+        <h2 className='text-xl tracking-wide text-center uppercase text-[#edfcfa] font-bold mb-2'>
+          {title}
+        </h2>
+        <p className='border-[1px] border-solid border-[#46cebe] rounded px-2 py-1 m-'>
+          {time} segundo{time > 1 ? 's' : ''}
+        </p>
+        <p
+          className='mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] 
         text-lg cursor-pointer hover:bg-[#051715]'>
-            <button onClick={hasStart ? handleStopTime : handleStartTime}>
-              {hasStart ? 'Parar' : 'Empezar'}
-            </button>
-          </p>
-          <p className=''></p>
-        </section>
-      </>
-    );
+          <button onClick={timeIsRunning ? handleStopTime : handleStartTime}>
+            {timeIsRunning ? 'Parar' : 'Empezar'}
+          </button>
+        </p>
+        <p className=''></p>
+      </section>
+    </>
+  );
 }
 
 export default Timer;
